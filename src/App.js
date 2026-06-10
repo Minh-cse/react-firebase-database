fc4243149ce48e45363d8bf7e818725a1eca17b6
Minh <104240648@student.vgu.edu.vn>
2026-06-10 23:35:55 +0700

    Initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      Minh
+    </div>
+  );
+}
+
+export default App;
