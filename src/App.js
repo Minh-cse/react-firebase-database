f4e68040ce20567e603cb38f7c659565c0a0589c
Minh <104240648@student.vgu.edu.vn>
2026-06-11 10:40:51 +0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Write from './components/Write'
+import Read from './components/Read'
 
 function App() {
   return (
     <div className="App">
-      Minh
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Write/>}/>
+          <Route path = "/write" element = {<Write/>}/>
+          <Route path = "/read" element = {<Read/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
