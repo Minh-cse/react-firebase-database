ff23c611450f80bb21ede3f3e04cd2d00cad8502
Minh <104240648@student.vgu.edu.vn>
2026-06-11 10:37:27 +0700

    Writing data to the server

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import app from "../firebaseConfig";
+import { getDatabase, ref, set, push } from "firebase/database";
 
 function Write() {
+    const [inputValue1, setInputValue1] = useState("");
+    const [inputValue2, setInputValue2] = useState("");
+
+    const saveData = () => {
+        const db = getDatabase(app);
+        const newDocRef = push(ref(db, "health/heartRate"));
+
+        set(newDocRef, {
+            PatientID: inputValue1,
+            heartRate: inputValue2
+        })
+        .then(() => {
+            alert("Data saved successfully!");
+            setInputValue1("");
+            setInputValue2("");
+        })
+        .catch((error) => {
+            alert("Error: " + error.message);
+        });
+    };
+
     return (
-        <div>Write</div>
-    )
+        <div>
+            <input
+                type="text"
+                value={inputValue1}
+                onChange={(e) => setInputValue1(e.target.value)}
+                placeholder="Patient ID"
+            />
+
+            <input
+                type="text"
+                value={inputValue2}
+                onChange={(e) => setInputValue2(e.target.value)}
+                placeholder="Heart Rate"
+            />
+
+            <br />
+
+            <button onClick={saveData}>Save Data</button>
+        </div>
+    );
 }
 
-export default Write
+export default Write;
